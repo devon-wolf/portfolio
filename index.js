303ef589b8e39536d61b7122ed6efc18fc626fc7
Devon Wolfkiel <devonwolf@tutanota.com>
2021-05-19 13:47:45 -0700

    Cleanup throughout

--- index.js
+++ index.js
@@ -4,7 +4,6 @@ import { chooseTheme } from './utils/render-utils.js';
 chooseTheme(getTheme());
 
 const themeToggle = document.getElementById('theme-toggle');
-// const pdfButton = document.querySelector('.download');
 
 themeToggle.addEventListener('click', () => {
 	const currentTheme = getTheme();
@@ -21,10 +20,3 @@ themeToggle.addEventListener('click', () => {
 	};
 	chooseTheme(getTheme());
 });
-
-// if(pdfButton) {
-// 	pdfButton.addEventListener('click', () => {
-// 		console.log('Thanks for checking out the console!');
-// 		alert('Sorry, this button is not yet active. But thanks for clicking!');
-// 	});
-// }
